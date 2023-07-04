562917d4b05acf96d8a9c51901c84fc06da28744
livschif <oliviaschif@gmail.com>
2023-07-03 22:53:04 -0500

    added password lenght prompt

--- develop/script.js
+++ develop/script.js
@@ -1,22 +1,32 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-//var length = (8);
-//var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-//var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-//var number = "0123456789";
-//var symbol = "@#$%^&*()";
-
 
 // Write password to the #password input
 
 
- function writePassword() {
- var password = generatePassword();
- var passwordText = document.querySelector("#password");
+// function writePassword() {
+   //var password = generatePassword();
+  // var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+ // passwordText.value = password;
 
- }
+ //}
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+//generateBtn.addEventListener("click", writePassword);
+
+var passlength = 0;
+var selpassopt = function() {
+    var passlengthopt = window.prompt("Enter from 8 to 128 only.");
+    if (passlengthopt >= 8 && passlengthopt <= 128) {
+      passlength = passlengthopt;
+      //return;
+    } else
+    window.alert("Select valid entry");
+
+    selpassopt();
+    }
+
+
+
+generateBtn.addEventListener("click", selpassopt);
