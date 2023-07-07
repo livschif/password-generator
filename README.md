48f042cf6d56503553118d7070b7ea3939d2909e
livschif <oliviaschif@gmail.com>
2023-07-06 23:19:05 -0500

    added decision making logic based on the user input

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# password-generator
+# Random Password Generator
+
+## Description
+
+This is a random password generator made for the KU Bootcamp. You can use this generator to create random passwords that include uppercase letters, lowercase letters, numbers, and special characters.
+
+## Installation
+
+Download file or clone through the Github repository "livschif/password-generator"
+
+## Mock up 
+
+![mockup photo](assets/mockup.png)
+
+## Links
+Link to Github repo:
+https://github.com/livschif/password-generator
+
+Link to deployed webpage:
+https://livschif.github.io/password-generator/
--- develop/script.js
+++ develop/script.js
@@ -1,51 +1,38 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
 
-
-//function writePassword() {
-   //var password = generatePassword();
-   //var passwordText = document.querySelector("#password");
-
- //passwordText.value = password;
-
- //}
-
-// Add event listener to generate button
+var passlengthopt;
 var speccharConfirm;
 var passnumConfirm;
 var uppercaseConfirm;
 var lowercaseConfirm;
 var passlength = 0;
 // Special characters 
-var specchar = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+var specchar = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
 // Numeric characters
 var passnum = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-// Alphabetical characters
+// Lowercase characters
 var passlett = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-// Space is for the Uppercase conversion
-var upperspace = [];
+// Uppercase letters
+var passlettupper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
 var options;
 
-var toUpper = function(o) {
-    return o.toUpperCase();
-}
-
-var passlettupper = passlett.map(toUpper);
 // start function to generate password
 var selpassopt = function() {
     var passlengthopt = window.prompt("Enter from 8 to 128 only.");
     if (!passlengthopt) {
         window.alert("Select valid entry!"); 
     }
-    else if (passlengthopt <= 8 && passlengthopt >= 128) {
+    else if (passlengthopt < 8 || passlengthopt > 128) {
         passlengthopt = window.prompt("Only enter 8 to 128 characters.");
-      //passlength = passlengthopt;
-      //return;
-      //redo order 
-    } else {
+      if (passlengthopt < 8 || passlengthopt > 128) {
+        selpassopt ();
+      }
+      
+    }
+        if (passlengthopt) {
         speccharConfirm = window.confirm("Would you like to use special character");
         passnumConfirm = window.confirm("Would you like to use numbers?");
         uppercaseConfirm = window.confirm("Would you like to use Uppercase letters?");
@@ -53,50 +40,82 @@ var selpassopt = function() {
     };
 
     if (!speccharConfirm && !passnumConfirm && !uppercaseConfirm && !lowercaseConfirm) {
-        choices = alert("Please select either: Uppercase, Lowercase, Special characters, or Numbers.");
+        options = alert("Please select either: Uppercase, Lowercase, Special characters, or Numbers.");
      }
     else if (speccharConfirm && passnumConfirm && uppercaseConfirm && lowercaseConfirm) {
-        choices = specchar.concat(passnum, passlettupper, passlett);
+        options = specchar.concat(passnum, passlettupper, passlett);
     } 
 
 
     else if (speccharConfirm && passnumConfirm && uppercaseConfirm) {
-        choices = specchar.concat(passnum, passlettupper);
-        //alert("your not selecting lowercase")
+        options = specchar.concat(passnum, passlettupper);
     }
     else if (speccharConfirm && passnumConfirm && lowercaseConfirm) {
-        choices = specchar.concat(passnum, passlett);
-       // alert("your not selecting uppercase")
+        options = specchar.concat(passnum, passlett);
     }
     else if (speccharConfirm && lowercaseConfirm && uppercaseConfirm) {
-        choices = specchar.concat(passlett, passlettupper);
-        //alert("your momma is a number")
+        options = specchar.concat(passlett, passlettupper);
     }
     else if (passnumConfirm && lowercaseConfirm && uppercaseConfirm) {
-        choices = passnum.concat(passlett, passlettupper);
-        //alert("alert" + choices)
+        options = passnum.concat(passlett, passlettupper);
     }
+
+
     else if (speccharConfirm && passnumConfirm) {
-        choices = specchar.concat(passnum);
+        options = specchar.concat(passnum);
     }
 
     else if (speccharConfirm && lowercaseConfirm) {
-        choices = specchar.concat(passlett);
+        options = specchar.concat(passlett);
     }
 
     else if (speccharConfirm && uppercaseConfirm) {
-        choices = specchar.concat(passlettupper);
+        options = specchar.concat(passlettupper);
     }
     else if (passnumConfirm && lowercaseConfirm) {
-        choices = passnum.concat(passlett);
+        options = passnum.concat(passlett);
     }
     else if (passnumConfirm && uppercaseConfirm)
     {
-        choices = passnum.concat(passlettupper);
+        options = passnum.concat(passlettupper);
     }
     else if (lowercaseConfirm && uppercaseConfirm)
     {
-        choices = passlett.concat(passlettupper);
+        options = passlett.concat(passlettupper);
     }
+
+
+    else if (uppercaseConfirm) {
+        options = (passlettupper);
+    }
+    else if (lowercaseConfirm) {
+        options = (passlett);
+    } 
+    else if (passnumConfirm) {
+        options = (passnum);
+    } 
+    else if (speccharConfirm) {
+        options = (specchar);
+    }  
+
+
+var password = [];
+
+for(var i = 0; i < passlengthopt; i++) {
+    var selectoptions = options[Math.floor(Math.random () * options.length)];
+    password.push(selectoptions);
 }
-generateBtn.addEventListener("click", selpassopt);
+ var pass = password.join("");
+ UserInput(pass);
+ return pass;
+
+};
+function UserInput(pass) {
+    document.getElementById("password").textContent = pass;
+}
+
+var passwordText = document.querySelector("#password");
+generateBtn.addEventListener("click", function() {
+   pass = selpassopt();
+   document.getElementById("password")
+});
