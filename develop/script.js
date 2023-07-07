c5842465077ce4c2d777deceaeb871b277d242e8
livschif <oliviaschif@gmail.com>
2023-07-06 23:29:28 -0500

    fixed typos on project

--- develop/script.js
+++ develop/script.js
@@ -11,7 +11,7 @@ var passlength = 0;
 // Special characters 
 var specchar = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
 // Numeric characters
-var passnum = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+var passnum = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 // Lowercase characters
 var passlett = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 // Uppercase letters
